d739fe38a95431c61969e045c5cabc7d8e15a824
tympush <tymur.pushnoy@gmail.com>
2025-05-22 13:32:58 +0200

    completed the activity but have page bugs

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,7 @@
 import { loadHeaderFooter } from "./utils.mjs";
 import { updateCartCount } from "./utils.mjs";
-import ProductData from "./ProductData.mjs";
-import ProductList from "./ProductList.mjs";
 
 // Wait for header/footer to load, then update cart count
 loadHeaderFooter().then(() => {
     updateCartCount();
 });
-
-const dataSource = new ProductData("tents");
-const element = document.querySelector(".product-list");
-const productList = new ProductList("Tents", dataSource, element);
-productList.init();
--- src/js/product.js
+++ src/js/product.js
@@ -1,18 +1,16 @@
-import { loadHeaderFooter } from "./utils.mjs";
-import { updateCartCount } from "./utils.mjs";
-import { getParam } from "./utils.mjs";
+import { loadHeaderFooter, updateCartCount, getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
-
-const dataSource = new ProductData("tents");
-const productID = getParam("product");
-
-const product = new ProductDetails(productID, dataSource);
-product.init();
 
 loadHeaderFooter().then(() => {
     updateCartCount();
 });
+
+const dataSource = new ProductData();
+const productID = getParam("product");
+
+const product = new ProductDetails(productID, dataSource);
+product.init();
 
 // // add to cart button event handler
 // async function addToCartHandler(e) {
